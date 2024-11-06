499221d129af5256b84111b271ff8faed307da88
Gia-gia9 <giadagelashvili@gmail.com>
2024-11-06 22:18:13 +0400

    sum

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let age = 30;
+
+
+if (age<=0 || age>120 ) {
+    console.log("არასწორი ასაკი")
+} else if (age <= 12) {
+    console.log("let ticket price $5")
+} else if (age <= 17) {
+    console.log("let ticket price $10")
+} else if (age <=64) {
+    console.log("let ticket price $15")
+} else if (age >= 65){
+    console.log("let ticket price $8")
+} 
